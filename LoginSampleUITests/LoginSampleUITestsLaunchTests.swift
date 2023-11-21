0e15d40bb4f61520daf3bd376fce2e3e5146de0a
prc-gllg <143255141+prc-gllg@users.noreply.github.com>
2023-11-21 18:06:58 +0800

    Initial Commit

---
+++ LoginSampleUITests/LoginSampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LoginSampleUITestsLaunchTests.swift
+//  LoginSampleUITests
+//
+//  Created by Pierce Gallego on 11/21/23.
+//
+
+import XCTest
+
+final class LoginSampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
